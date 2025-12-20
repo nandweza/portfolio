14c701ae0e74c044f3aa33e49dc9dc70758f4258
nandweza <allannandweza@gmail.com>
2025-12-19 20:08:09 -0700

    updated home page and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import "./navbar.css";
 import {NavLink } from "react-router-dom";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Navbar = () => {
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <NavLink to="/" className="navbar-brand px-5">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+            <NavLink to="/" className="navbar-brand">
                 <h1>AK</h1>
             </NavLink>
             <button 
-                className="navbar-toggler mx-5" 
+                className="navbar-toggler" 
                 type="button" 
                 data-bs-toggle="collapse" 
                 data-bs-target="#navbarSupportedContent" 
@@ -19,7 +23,8 @@ const Navbar = () => {
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto justify-content-end px-5">
+                <div className="ms-auto d-flex flex-column flex-lg-row aligns-items-start align-items-lg-center gap-lg-4 gap-3 pt-lg-0">
+                <ul className="navbar-nav d-flex flex-column flex-lg-row gap-lg-3 text-start">
                     <li className="nav-item">
                         <NavLink 
                             to="/" 
@@ -51,6 +56,25 @@ const Navbar = () => {
                         </NavLink>
                     </li>
                 </ul>
+                </div>
+                <div className="nav-divider d-none d-lg-block"></div>
+                <div className="d-flex align-items-center">
+                    <a href="" target="_blank" rel="noreferrer">
+                        <span className="nav-icon-wrapper" data-bs-toggle="tooltip" title="LinkedIn">
+                            <LinkedInIcon />
+                        </span>
+                    </a>         
+                    <a href="" target="_blank" rel="noreferrer">
+                        <span className="nav-icon-wrapper" data-bs-toggle="tooltip" title="GitHub">
+                            <GitHubIcon />
+                        </span>
+                    </a> 
+                    <a href="" target="_blank" rel="noreferrer">
+                        <span className="nav-icon-wrapper" data-bs-toggle="tooltip" title="Contact Me">
+                            <MailIcon />
+                        </span>
+                    </a> 
+                </div>
             </div>
         </nav>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,42 +2,44 @@ import "./home.css";
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import MailIcon from '@mui/icons-material/Mail';
 
-const Home = () => (
-    <div>
-        <Navbar />
-        <div className="container">
-            <div className="left item">
-                <h1>I'am Allan Kindarara</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                </p>
-                <button className="">
-                    My Resume
-                </button>
-                <div className="icons-flex">
-                    <span>
-                        <LinkedInIcon sx={{fontSize: {xs: 25, md: 40} }} />
-                    </span>
-                    <span>
-                        <GitHubIcon sx={{fontSize: {xs: 25, md: 40} }} />
-                    </span>
-                    <span>
-                        <MailIcon sx={{fontSize: {xs: 25, md: 40} }} />
-                    </span>
+const Home = () => {
+    
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="row min-vh-100 align-items-center">
+                    <div className="col-12 col-md-7 d-flex flex-column justify-content-center align-content-center mb-4 mb-md-0 animated-text">
+                        <div className="flex-column justify-content-center align-item-center">
+                            <h1>I'am Allan Kindarara</h1>
+                            <h3>Software Engineer | FullStack Developer</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            </p>
+                            <input 
+                                type="button" 
+                                value="My Resume" 
+                                className="btn btn-primary btn-lg mb-3 shadow" 
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-5 d-flex justify-content-center align-items-center position-relative">
+                        <img 
+                            src="https://nandweza.github.io/allan/images/allan.png" 
+                            alt="hero image" 
+                            width="100%" 
+                            className="img-fluid rounded-circle shadow hero-image" 
+                            style={{maxWidth: "300px" }}
+                        />
+                    </div>
                 </div>
             </div>
-            <div className="card right item">
-                <img src="https://nandweza.github.io/allan/images/allan.png" alt="Allan's photo" width="100%" />
-            </div>
-        </div>
-        <Footer />
-    </div>
-)
+            <Footer />
+        </>
+    );
+};
 
 export default Home;
