dcf0d5c95536df6aaa8abe4c6f2be85f09039388
nandweza <allannandweza@gmail.com>
2025-12-30 13:56:05 -0700

    fixed react router issue causing blank page on deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/portfolio'>
     <App />
   </BrowserRouter>
 )
