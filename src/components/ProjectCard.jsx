b722a7a4816f244394971c5778834555fb9e4cbc
nandweza <allannandweza@gmail.com>
2025-12-29 19:42:26 -0700

    updated project data

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -15,7 +15,7 @@ const ProjectCard = (
                 </div>
                 <div className="col-md-6">
                     <div className="card-body d-flex flex-column h-100">
-                        <a href="">
+                        <a href={demoUrl} target="_blank" rel="noreferrer">
                             <h4 className="card-title fw-semibold">{title}</h4>
                         </a>
                         <p className="card-text text-muted flex-grow-1">
@@ -23,16 +23,16 @@ const ProjectCard = (
                         </p>
                         <div className="mb-3">
                             {techStack.map((tech, i) => (
-                                <span key={i} className="badge bg-dark me-2">
+                                <span key={i} className="badge bg-secondary me-2">
                                     {tech}
                                 </span>
                             ))}
                         </div>
                         <div className="d-flex gap-2 mt-auto">
-                            <a href={demoUrl} className="btn btn-danger btn-sm">
+                            <a href={demoUrl} target="_blank" rel="noreferrer" className="btn btn-danger btn-sm">
                                 Live Demo
                             </a>
-                            <a href={codeUrl} className="btn btn-outline-dark btn-sm">
+                            <a href={codeUrl} target="_blank" rel="noreferrer" className="btn btn-outline-dark btn-sm">
                                 GitHub
                             </a>
                         </div>
--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -1,58 +1,63 @@
+import Oftac from "../assets/oftac.png";
+import PortfolioOne from "../assets/portfolio-one.png";
+import newPortfolio from "../assets/new-portfolio.png";
+
+
 const ProjectData = [
     {
-        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
-        alt: "project one img",
-        title: "Project One",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        techStack : [ "JavaScript", "HTML", "CSS", "Git"],
-        demoUrl: "#",
-        codeUrl: "#"
+        imgUrl: Oftac, 
+        alt: "oftac.org image",
+        title: "OFTAC.ORG - UGANDA",
+        description: "A react App for Organization For Transforming African Communities, a small refugee led organization in Kyangwali Refugee camp",
+        techStack : [ "JavaScript", "HTML", "CSS", "React", "Material UI", "Bootstrap5", "Nodejs", "Expressjs", "MongoDB", "Render"],
+        demoUrl: "https://oftac.onrender.com/#/",
+        codeUrl: "https://github.com/nandweza/oftac"
     },
     {
-        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
-        alt: "project two img",
-        title: "Project Two",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        techStack : [ "Python", "HTML", "Sass", "Git", "Flask", "React", "JavaScript", "TypeScript", "MongoDB"],
-        demoUrl: "#",
-        codeUrl: "#"
+        imgUrl: Oftac, 
+        alt: "oftac.org image",
+        title: "OFTAC.ORG API",
+        description: "API for Organization For Transforming African Communities built with nodejs, express, and typescript.",
+        techStack : [ "Nodejs", "Expressjs", "Mongoose", "nodemon", "bcrypt", "jwt-authentication", "multer", "TypeScript", "MongoDB", "nodemailer", "ts-node"],
+        demoUrl: "https://oftac.onrender.com/#/",
+        codeUrl: "https://github.com/nandweza/oftac-backend"
     },
     {
-        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
-        alt: "project three img",
-        title: "Project Three",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        techStack : [ "Java", "XML", "CSS", "Git", "JAVAFX"],
-        demoUrl: "#",
-        codeUrl: "#"
+        imgUrl: PortfolioOne, 
+        alt: "my first portfolio home page image",
+        title: "First Porfolio Website",
+        description: "My first personal portfolio website built with html5, CSS3, and Bootstrap5.",
+        techStack : [ "HTML", "CSS", "Bootstrap5", "Github Pages",],
+        demoUrl: "https://nandweza.github.io/allan/",
+        codeUrl: "https://github.com/nandweza/allan"
     },
     {
-        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
-        alt: "project two img",
-        title: "Project Two",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        techStack : [ "Python", "HTML", "Sass", "Git", "Flask", "React", "JavaScript", "TypeScript", "MongoDB"],
+        imgUrl: newPortfolio, 
+        alt: "new portfolio homepage image",
+        title: "My Porfolio React App",
+        description: "My Personal portfolio website built with React + Vite, showcases my projects and skills.",
+        techStack : [ "JavaScript", "HTML", "CSS", "React", "Bootstrap5", "Material UI", "Vite"],
         demoUrl: "#",
-        codeUrl: "#"
+        codeUrl: "https://github.com/nandweza/portfolio"
     },
-    {
-        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
-        alt: "project two img",
-        title: "Project Two",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        techStack : [ "Python", "HTML", "Sass", "Git", "Flask", "React", "JavaScript", "TypeScript", "MongoDB"],
-        demoUrl: "#",
-        codeUrl: "#"
-    },
-    {
-        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
-        alt: "project two img",
-        title: "Project Two",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        techStack : [ "Python", "HTML", "Sass", "Git", "Flask", "React", "JavaScript", "TypeScript", "MongoDB"],
-        demoUrl: "#",
-        codeUrl: "#"
-    },  
+    // {
+    //     imgUrl: Go, 
+    //     alt: "GO USETECH",
+    //     title: "GO USETECH home page image",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //     techStack : [ "Javascript", "ejs", "CSS3", "Git", "Nodejs", "Expressjs", "oauth2", "Mongoose", "MongoDB"],
+    //     demoUrl: "#",
+    //     codeUrl: "https://github.com/nandweza/Go-use_Tech"
+    // },
+    // {
+    //     imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.unsell.design%2Fwp-content%2Fuploads%2F2023%2F08%2F633617353_Template-Featured-image.jpg&f=1&nofb=1&ipt=0b2108507802b23e51b2c8cf035982908e0897693f4c66c69860a75741f3d58f", 
+    //     alt: "project two img",
+    //     title: "Project Two",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //     techStack : [ "Python", "HTML", "Sass", "Git", "Flask", "React", "JavaScript", "TypeScript", "MongoDB"],
+    //     demoUrl: "#",
+    //     codeUrl: "#"
+    // },  
 ];
 
 export default ProjectData;
