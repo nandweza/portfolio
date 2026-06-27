f124d7b2fc085affeef0bb55c62dc7d8cfdcd423
Allan Kindarara <allannandweza@gmail.com>
2026-06-27 12:16:45 -0700

    added resume link

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,24 +16,27 @@ const Home = () => {
                             <h1><span className="text-danger">I'm</span> Allan Kindarara</h1>
                             <h3><span className="text-danger">Software Engineer</span> | <span className="text-danger">FullStack Developer</span></h3>
                             <p>
-                                Hi, I'm Allan, a detail-oriented Software Engineer and FullStack Developer passionate about building 
-                                innovative solutions, solving complex problems, and continously learning new technologies. 
-                                I am currently an undergraduate Computer Science student at College of Western Idaho.
+                                Hi, I'm Allan, a detail-oriented Software Engineer and FullStack Developer passionate about building
+                                innovative solutions, solving complex problems, and continously learning new technologies.
+                                I am currently an undergraduate Computer Science student at Boise State University.
                             </p>
-                            <input 
-                                type="button" 
-                                value="RESUME" 
-                                className="btn btn-danger btn-lg mb-3 shadow fw-bold shadow-sm" 
-                            />
+                            <a
+                                href="https://docs.google.com/document/d/1BCDMl7r7OC7jqQ9YEA4blLL8cW6n4wJ6/edit?usp=sharing&ouid=111459362459709066324&rtpof=true&sd=true"
+                                type="button"
+                                target="_blank"
+                                className="btn btn-danger btn-lg mb-3 shadow fw-bold shadow-sm"
+                            >
+                                RESUME
+                            </a>
                         </div>
                     </div>
                     <div className="col-12 col-md-5 d-flex justify-content-center align-items-center position-relative">
-                        <img 
+                        <img
                             src={Allan}
-                            alt="hero image" 
-                            width="100%" 
-                            className="img-fluid rounded-circle shadow hero-image" 
-                            style={{maxWidth: "300px" }}
+                            alt="hero image"
+                            width="100%"
+                            className="img-fluid rounded-circle shadow hero-image"
+                            style={{ maxWidth: "300px" }}
                         />
                     </div>
                 </div>
